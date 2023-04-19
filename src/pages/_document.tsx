d74ad80d442037d8251151865304b63644696115
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2023-04-19 14:00:58 -0700

    added tawk.to

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,21 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Poppins:wght@400;500;700;900&display=swap" rel="stylesheet" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/643df3d44247f20fefec355f/1gu900tsr';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </Head>
       <body className='bg-white font-poppins text-lg'>
         <Main />
