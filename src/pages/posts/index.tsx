0ff2467d36117569e164683a4d02ac26ee761ca8
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2023-04-19 14:34:45 -0700

    updated revalidate to 1 minute

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -143,6 +143,6 @@ export const getStaticProps: GetStaticProps = async () => {
       page: response.page,
       totalPage: response.total_pages
     },
-    revalidate: 60 * 30 // update every 30 minutes
+    revalidate: 60 * 1 // update every 30 minutes
   }
 }
